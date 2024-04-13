0f7e2ffa0f6fd624e8c9060df8fc0c18dad14829
Cathy Nawagirwa <cathynawagirwa@gmail.com>
2024-04-13 10:32:40 +0300

    added event listener to the buttons.

---
+++ drumkit.js
@@ -0,0 +1,8 @@
+var numberOfDrumButtons = document.querySelectorAll(".drum").length;
+for (var i= 0; i<numberOfDrumButtons; i++){
+
+document.querySelector("button").addEventListener("click",function handleClick(){
+    alert("I got clicked!");
+});
+
+}
