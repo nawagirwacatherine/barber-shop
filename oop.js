5b59405e692c65955afc69e62fd247b4ce99ae51
Cathy Nawagirwa <cathynawagirwa@gmail.com>
2024-04-20 07:43:08 +0300

    added subclass high speed train

--- oop.js
+++ oop.js
@@ -20,8 +20,40 @@ class Train {
     }
 }
 
+class HighSpeedTrain extends Train {
+    constructor(passengers,highSpeedOn,color, lightsOn){
+        super(color,lightsOn);
+        this.passengers = passengers;
+        this.highSpeedOn = highSpeedOn;
+    }
+    toggleHighSpeed(){
+        this.highSpeedOn = !this.highSpeedOn;
+        console.log('High speed status:', this.highSpeedOn);
+    }
+
+    toggleLights(){
+        super.toggleLights();
+        super.lightsStatus();
+        console.log('Lights are 100% operational.');
+    }
+
+}
+
+var myFirstTrain = new Train ('red',false);
+console.log(myFirstTrain);
+var mySecondTrain = new Train('blue',false);
+var myThirdTrain  = new Train('blue',false);
+
 var train4 =new Train('red', false);
 train4.toggleLights();
 train4.lightsStatus();
 train4.getSelf();
 train4. getPrototype();
+
+var train5 = new Train('blue',false);
+var highSpeed1 = new HighSpeedTrain(200,false,'green',false);
+
+train5.toggleLights();
+train5.lightsStatus();
+highSpeed1.toggleLights();
+
