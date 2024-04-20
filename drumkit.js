dd8e7c29b43cbf464b5bdf1f80c4932a3b6f4df6
Cathy Nawagirwa <cathynawagirwa@gmail.com>
2024-04-20 06:54:33 +0300

    added variable train4 to Train object

--- drumkit.js
+++ drumkit.js
@@ -1,8 +1,9 @@
 var numberOfDrumButtons = document.querySelectorAll(".drum").length;
 for (var i= 0; i<numberOfDrumButtons; i++){
 
-document.querySelector("button").addEventListener("click",function handleClick(){
-    alert("I got clicked!");
+document.querySelectorAll(".drum")[i].addEventListener("click",function handleClick(){
+  var audio = new Audio("sounds/sound2.m4a");
+  audio.play();
 });
 
 }
---
+++ oop.js
@@ -0,0 +1,27 @@
+class Train {
+    constructor(color,lightsOn){
+        this.color = color;
+        this.lightsOn = lightsOn;
+
+
+    }
+    toggleLights(){
+        this.lightsOn = !this.lightsOn;
+    }
+    lightsStatus(){
+        console.log('Lights on?',this.lightsOn);
+    }
+    getSelf(){
+        console.log(this);
+    }
+    getPrototype(){
+        var proto = Object.getPrototypeOf(this);
+        console.log(proto);
+    }
+}
+
+var train4 =new Train('red', false);
+train4.toggleLights();
+train4.lightsStatus();
+train4.getSelf();
+train4. getPrototype();
